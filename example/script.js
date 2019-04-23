6630bf5730413ef7886d95baf0f153bd063228df
BadHomer <40883946+BadHomer@users.noreply.github.com>
2019-04-23 11:20:14 +0300

    Update script.js

--- example/script.js
+++ example/script.js
@@ -1,4 +1,16 @@
 let canvas = document.getElementById("sandbox"),
 context = canvas.getContext("2d");
+    square, circle;
+square = new Path2D();
 
-context.fillRect(50,25,200,250);
+square.moveTo(50,50);
+square.lineTo(250,50);
+square.lineTo(250,250);
+square.lineTo(50,250);
+square.lineTo(50,50);
+context.stroke(square);
+
+circle = new Path2D();
+circle.arc(150,150,100,0,2* Math.PI);
+
+context.fill(circle);
