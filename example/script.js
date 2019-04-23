37ba3c86436b68e3829b3ee05696632079295a1b
BadHomer <40883946+BadHomer@users.noreply.github.com>
2019-04-23 12:16:56 +0300

    Update script.js

--- example/script.js
+++ example/script.js
@@ -1,16 +1,47 @@
 let canvas = document.getElementById("sandbox"),
     context = canvas.getContext("2d"),
-    square, circle;
-square = new Path2D();
+    line, circle;
+    del = new Path2D();
+    let r = 300 / 2;
+    for(let d = 0;d < 60; ++d) {
+        let angle = (d / 60) * (2 * Math.PI);
+        let pX = Math.cos(angle) * r;
+        let pY = -Math.sin(angle) * r;
+        let qX = 0.9 * pX;
+        let qY = 0.9 * pY;
+        pY = - pY;
+        qY= - qY;
+        pX += r; pY += r;
+        qX += r; qY += r;
+        del.moveTo(pX,pY);
+        del.lineTo(qX,qY);
+        context.stroke(del);
+    }
 
-square.moveTo(50,50);
-square.lineTo(250,50);
-square.lineTo(250,250);
-square.lineTo(50,250);
-square.lineTo(50,50);
-context.stroke(square);
+let date = new Date();
+let hours = date.getHours();
+let minute = date.getMinutes();
+let seconds = date.getSeconds();
+seconds = 30;
+minutes = 15;
+hours = 20;
 
+secondsAngle = Math.PI / 2 - (seconds * 60) * (2 * Math.PI);
+minutesAngle = Math.PI / 2 - (minutes * 60) * (2 * Math.PI);
+hoursAngle = Math.PI / 2 - ((hours % 12) / 12) * (2 * Math.PI);
+
+line = new Path2D();
+
+let pX = 0.9 * Math.cos(secondsAngle) * r;
+let pY = 0.9 * -Math.sin(secondsAngle) * r;
+pY = - pY;
+pX += r; 
+pY += r;
+line.moveTo(150,150);
+line.lineTo(pX,pY);
+context.stroke(line);
 circle = new Path2D();
-circle.arc(150,150,100,0,2* Math.PI);
+circle.arc(150,150,150,0,2* Math.PI);
 
 context.stroke(circle);
+
